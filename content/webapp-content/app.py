35d73e89de4f998720a75a235d18b9f38a3a441d
omar <omarelkassar8@gmail.com>
2022-04-20 12:19:36 +0200

    try web app

--- content/webapp-content/app.py
+++ content/webapp-content/app.py
@@ -1,7 +1,32 @@
+'''
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    exec(open("./primernumb.py").read())
     return "Hello, this is a sample Python Web App running on Flask Framework new version"
+'''
+
+from flask import Flask , request
+from function_name import primeNum ,sumNum
+app = Flask(__name__)
+
+#http://127.0.0.1:5000/prime?num=30
+@app.route('/prime')
+def prime():
+    num = request.args['num']
+   # return 'The value is: ' + num
+    msg = primeNum(int(num))
+    return  num + ' ' +msg
+
+       
+#http://127.0.0.1:5000/sum?num1=30&num2=4
+@app.route('/sum')
+def sum():
+    num1 = request.args['num1']
+    num2 = request.args['num2']
+   # return 'The value is: ' + num
+    num = sumNum(int(num1), int(num2))
+    return  'The result is ' + str(num) 
---
+++ python-provas/main.py
@@ -0,0 +1,3 @@
+#import check_primer_number.py
+#execfile('check_primer_number.py')
+exec(open("./check_primer_number.py").read())
