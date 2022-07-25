0db6864d659c2773aca09a889c97469e7ae0cf68
omar <omarelkassar8@gmail.com>
2022-07-25 22:53:41 +0200

    mm

--- content/webapp-content/app.py
+++ content/webapp-content/app.py
@@ -13,26 +13,17 @@ from flask import Flask , request ,render_template
 from functions import primeNum ,sumNum
 app = Flask(__name__)
 
-#http://127.0.0.1:5000/prime?num=30
-@app.route('/prime')
-def prime():
-    num = request.args['num']
-   # return 'The value is: ' + num
-    msg = primeNum(int(num))
-    return  num + ' ' +msg
 
-       
-#http://127.0.0.1:5000/sum?num1=30&num2=4
-@app.route('/sum')
-def sum():
-    num1 = request.args['num1']
-    num2 = request.args['num2']
-   # return 'The value is: ' + num
-    num = sumNum(int(num1), int(num2))
-    return  'The result is ' + str(num) 
+@app.route('/exponential')
+def exponential():
+    return render_template('exponential.html')
 
 
-#http://127.0.0.1:5000/
+@app.route('/squad')
+def squad():
+    return render_template('squad.html')
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
