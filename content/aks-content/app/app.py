159c62a8e7e814777ecad6071417cec54f7afd5b
omar <omarelkassar8@gmail.com>
2022-07-25 22:01:08 +0200

    aks-docker-file

--- content/aks-content/app/app.py
+++ content/aks-content/app/app.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import pypyodbc
 import os
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('./index.html')
+def expo():
+        server = 'tcp:proj-dev-sql.database.windows.net'
+        database = 'proj-dev-sqldb'
+        username = 'omar1'
+        password = 'Kassar@14689'
+        num = request.args.get('num', default = 1, type = int)
+        cnxn = pypyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+        cursor = cnxn.cursor()
+        query = "select result from exponentiation where num=" + str(num)
+        cursor.execute(query)
+        row = cursor.fetchone()
+        result = 0
+        if cursor.rowcount == 0:
+         result = num ** num
+         query = """INSERT INTO exponentiation VALUES (?, ?)"""
+         cursor.execute(query,(num,result))
+         cnxn.commit()
+         cursor.close()
+         cnxn.close()
+        else:
+         print(row)
+         result= row
+        result = str(result)
+        result = result.replace(",)" , "")
+        result = result.replace("(" , "")
+        return str(result)
 
 
 if __name__ == "__main__":
