0a7495ec1e88f6c0f420fc7342c83e7837addccb
omartfg626 <omartfg626@gmail.com>
2022-09-22 06:02:22 +0200

    push

--- content/webapp-content/app.py
+++ content/webapp-content/app.py
@@ -1,13 +1,4 @@
-'''
-from flask import Flask
-app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    exec(open("./primernumb.py").read())
-    return "Hello, this is a sample Python Web App running on Flask Framework new version"
-'''
 from flask import Flask , request ,render_template
 import requests
 from functions import primeNum ,sumNum
@@ -15,16 +6,16 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/exponential')
-def exponential():
-    func_url =  os.environ['func_url']
-    func_code =  os.environ['func_code']
-    return render_template('exponential.html', funcUrl=func_url, funcCode=func_code )
-
-
 @app.route('/squad')
 def squad():
-    return render_template('squad.html')
+    func_url =  os.environ['func_url']
+    func_code =  os.environ['func_code']
+    return render_template('squad.html', funcUrl=func_url, funcCode=func_code )
+
+
+@app.route('/exponential')
+def exponential():
+    return render_template('exponential.html')
 
 '''
 @app.route('/aks')
